ac92b6cc32f37a50ce0786f2b3bd9c510a2fb066
Gustah-Py <gustavo233almeida@gmail.com>
2024-07-31 07:51:11 -0300

    Add files via upload

---
+++ Site Link/Webbrowser.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+# URL do site que você deseja abrir
+url = "https://www.linkedin.com/in/gustavo-marques-de-almeida-5755202a6/"
+
+# Abrir o site no navegador padrão
+webbrowser.open(url)
