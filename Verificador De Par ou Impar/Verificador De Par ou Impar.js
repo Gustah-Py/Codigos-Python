61e6faf8b3ff8e44a520382f93138365d2ecc074
Gustah-Py <gustavo233almeida@gmail.com>
2024-07-31 08:06:37 -0300

    Delete Verificador De Par ou Impar directory

--- Verificador De Par ou Impar/Verificador De Par ou Impar.js
+++
@@ -1,9 +0,0 @@
-function verificarParOuImpar(numero) {
-    if (numero % 2 === 0) {
-        return 'O número é par';
-    } else {
-        return 'O número é ímpar';
-    }
-}
-
-console.log(verificarParOuImpar(7)); // Output: O número é ímpar
